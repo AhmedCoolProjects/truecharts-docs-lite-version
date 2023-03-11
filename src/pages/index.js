7e59c24b0f92be7d67346e41456546c6c20934c3
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2023-03-11 17:33:35 +0100

    still footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,120 @@
-import React from "react";
-import clsx from "clsx";
+import React, { useEffect, useRef, useState } from "react";
+import { AiOutlineDoubleRight } from "react-icons/ai";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import ProgressBar from "@site/src/components/ProgressBar";
+import HomepageCard from "@site/src/components/HomepageCard";
+import clsx from "clsx";
+
+const data = [
+  {
+    Svg: require("@site/static/img/easy-to-use-icon.svg").default,
+    SvgHover: require("@site/static/img/easy-to-use-icon.svg").default,
+    title: "Easy to Deploy",
+    description:
+      "TrueCharts features a neatly organized catalog of Apps for TrueNAS SCALE.",
+    link: "",
+  },
+  {
+    Svg: require("@site/static/img/file-settings-icon.svg").default,
+    SvgHover: require("@site/static/img/file-settings-icon.svg").default,
+    title: "Consistent Ecosystem",
+    description:
+      "All TrueCharts Apps, are build upon the same solid foundation. All featuring the same deployment experience",
+    link: "",
+  },
+  {
+    Svg: require("@site/static/img/shield-heart-icon.svg").default,
+    SvgHover: require("@site/static/img/shield-heart-icon.svg").default,
+    title: "Stability",
+    description:
+      "TrueCharts has stability as a prime importance: What is running, should stay running.",
+    link: "",
+  },
+];
 
 function HomepageHeader() {
+  const progressRef = useRef(null);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const progressBar = progressRef.current.querySelector(".progress-bar");
+    const progressValue = progressRef.current.querySelector(".progress-value");
+
+    const updateProgress = () => {
+      setProgress((prevProgress) => {
+        const newProgress = prevProgress + 1;
+        progressBar.style.width = `${newProgress}%`;
+        progressValue.textContent = `Goal: €${newProgress + 430}`;
+
+        if (newProgress === 70) {
+          clearInterval(interval);
+        }
+
+        return newProgress;
+      });
+    };
+
+    const interval = setInterval(updateProgress, 30);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <br></br>
-        <div className={styles.buttons}>
+    <header>
+      <div className={styles.container}>
+        <h1>{siteConfig.title}</h1>
+        <p>{siteConfig.tagline}</p>
+
+        <div className={styles.animated_button}>
           <Link
-            className="button button--secondary button--lg"
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+            }}
             to="/manual/guides/Important-MUST-READ"
           >
-            TrueNAS SCALE - 5min ⏱️
+            <span></span>
+            <h2>TrueNAS SCALE</h2>
           </Link>
         </div>
-        <h3>
-          <br></br>
-          <b>---</b>
-          <br></br>
-          <br></br>
-          <br></br>
-          Monthly Funding goal:
-          <br></br>
-        </h3>
-        <ProgressBar bgcolor="#38b856" completed="70" goal="500" />
-        <br></br>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/sponsor">
-            Sponsor TrueCharts
-          </Link>
+
+        <p>Monthly Funding goal:</p>
+        <div className="progress" ref={progressRef}>
+          <div className="progress-bar">
+            <div className="progress-value"></div>
+          </div>
+        </div>
+
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-end",
+          }}
+        >
+          <div
+            style={{
+              marginTop: 32,
+            }}
+            className={styles.animated_button}
+          >
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "inherit",
+              }}
+              to="/sponsor"
+            >
+              <span></span>
+              <h2>Sponsor TrueCharts</h2>
+            </Link>
+          </div>
         </div>
       </div>
     </header>
@@ -52,8 +129,12 @@ export default function Home() {
       description="Your source For TrueNAS SCALE Apps<head />"
     >
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+      <main className={styles.container}>
+        <div className={styles.grid}>
+          {data.map((cardItem) => (
+            <HomepageCard {...cardItem} key={cardItem.title} />
+          ))}
+        </div>
       </main>
     </Layout>
   );
